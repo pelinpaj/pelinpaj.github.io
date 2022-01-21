617492eb27d5eda6f3324ed2be81a9cd68ec9427
pelinpaj <irosarum@gmail.com>
2022-01-21 16:00:51 +0100

    unosvisor

---
+++ unosvisor/www/video-new.js
@@ -0,0 +1,74 @@
+window.onload = function() {
+
+	// Video
+	var video = document.getElementById("video");
+
+	// Buttons
+	var playButton = document.getElementById("play-pause");
+	var muteButton = document.getElementById("mute");
+	var backButton = document.getElementById("back");
+
+	// Sliders
+	var seekBar = document.getElementById("seek-bar");
+	var volumeBar = document.getElementById("volume-bar");
+
+
+	// Event listener for the play/pause button
+	video.addEventListener("click", function() {
+	    alert("Hello! I am an alert box!!");
+	})
+
+	    // Event listener for the play/pause button
+	playButton.addEventListener("click", function() {
+		if (video.paused == true) {
+			// Play the video
+			video.play();
+
+			// Update the button text to 'Pause'
+			playButton.innerHTML = "Pause";
+		} else {
+			// Pause the video
+			video.pause();
+
+			// Update the button text to 'Play'
+			playButton.innerHTML = "Play";
+		}
+	});
+
+
+	// Event listener for the back button
+	backButton.addEventListener("click", function() {
+	  window.history.back();
+	});
+
+
+	// Event listener for the seek bar
+	seekBar.addEventListener("change", function() {
+		// Calculate the new time
+		var time = video.duration * (seekBar.value / 100);
+
+		// Update the video time
+		video.currentTime = time;
+	});
+
+	
+	// Update the seek bar as the video plays
+	video.addEventListener("timeupdate", function() {
+		// Calculate the slider value
+		var value = (100 / video.duration) * video.currentTime;
+
+		// Update the slider value
+		seekBar.value = value;
+	});
+
+	// Pause the video when the seek handle is being dragged
+	seekBar.addEventListener("mousedown", function() {
+		video.pause();
+	});
+
+	// Play the video when the seek handle is dropped
+	seekBar.addEventListener("mouseup", function() {
+		video.play();
+	});
+
+}
